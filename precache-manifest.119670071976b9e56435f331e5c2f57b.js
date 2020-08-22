6f582324bfb14346fe162f41004890ffc5f97e1b
Stephen Roddy <roddyst@tcd.ie>
2020-08-22 15:58:24 +0100

    Updates

---
+++ precache-manifest.119670071976b9e56435f331e5c2f57b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7bdb3eba1c11b5b5613e84da92108ed",
+    "url": "/NASA---APOD---Proj/index.html"
+  },
+  {
+    "revision": "1e81d0d8cdfcbfc55618",
+    "url": "/NASA---APOD---Proj/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2974950ddd48506f0b09",
+    "url": "/NASA---APOD---Proj/static/js/2.a52fe7a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NASA---APOD---Proj/static/js/2.a52fe7a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e81d0d8cdfcbfc55618",
+    "url": "/NASA---APOD---Proj/static/js/main.5a837f27.chunk.js"
+  },
+  {
+    "revision": "ecccf4baa63b36f6cf48",
+    "url": "/NASA---APOD---Proj/static/js/runtime-main.beacb20d.js"
+  }
+]);
